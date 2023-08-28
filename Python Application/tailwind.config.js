e03a593642d7fce02960e4ff18ae866114170d30
thehyperpineapple <raghav.sampath2003@gmail.com>
2023-08-28 15:10:58 +0530

    Create: Table for Data Viewing

--- Python Application/tailwind.config.js
+++ Python Application/tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ["./templates/*.html",
             "./static/src/**/*.js",
             "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      grotesk: ['Space Grotesk', 'sans-serif'],
+      sans: ['Poppins', 'sans-serif'],
+    },
+    colors: {
+      'darkb': '#00001c',
+      'frost': '#fcfbfc',
+      'rice': '#fbf5ef',
+    }
+    
   },
   plugins: [
     require("flowbite/plugin")
