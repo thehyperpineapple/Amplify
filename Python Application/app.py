2e2a84b3da8baef36a6b8c8de7b56dfb94211c69
thehyperpineapple <raghav.sampath2003@gmail.com>
2023-08-30 16:08:14 +0530

    Integrated ChartJS and created Dashboard page

--- Python Application/app.py
+++ Python Application/app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, jsonify
 import os
 from dotenv import find_dotenv,load_dotenv
 import thingspeak
-
+from subprocess import Popen, PIPE
 import json
 
 # Fetch environment variables
@@ -17,6 +17,8 @@ read_key = os.getenv("READ_API_KEY")
 channel = thingspeak.Channel(id=channel_id, api_key = read_key)
 response = channel.get({"results": 1000})
 response = json.loads(response)
+
+
 
 # Flask App
 app = Flask(__name__)
@@ -47,6 +49,51 @@ def home():
             net_current_list.append(response['feeds'][entry]['field8'])
 
     return render_template("index.html", length = len(unit_list), unit_list = unit_list[::-1], date_list = date_list[::-1], time_list = time_list[::-1], threshold_list = threshold_list[::-1], red_line_list = red_line_list[::-1], blue_line_list = blue_line_list[::-1], yellow_line_list = yellow_line_list[::-1], net_current_list = net_current_list[::-1])
+
+@app.route("/dashboard",methods=['GET'])
+def dashboard():
+    # Define Plot Data
+    labels = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+    ]
+ 
+    data = [0, 10, 15, 8, 22, 18, 25]
+ 
+    
+    unit_list, time_list, date_list, threshold_list, red_line_list, blue_line_list, yellow_line_list, net_current_list = [], [], [], [], [], [], [], []
+
+    unit = response['channel']['field1']
+    time = response['channel']['field2']
+    date = response['channel']['field3']
+    threshold = response['channel']['field4']
+    red_line = response['channel']['field5']
+    blue_line = response['channel']['field6']
+    yellow_line = response['channel']['field7']
+    net_current = response['channel']['field8']
+    entries = response['channel']['last_entry_id']
+
+    for entry in range(0, len(response['feeds'])):
+        unit_list.append(response['feeds'][entry]['field1'])
+        time_list.append(response['feeds'][entry]['field2'])
+        date_list.append(response['feeds'][entry]['field3'])
+        threshold_list.append(response['feeds'][entry]['field4'])
+        red_line_list.append(response['feeds'][entry]['field5'])
+        blue_line_list.append(response['feeds'][entry]['field6'])
+        yellow_line_list.append(response['feeds'][entry]['field7'])
+        net_current_list.append(response['feeds'][entry]['field8'])
+            
+    # Return the components to the HTML template
+    # return render_template('dashboard.html',
+    #     data=data,
+    #     labels=labels,
+    # )
+    return render_template("dashboard.html", unit_list = unit_list, date_list = date_list[-30:], time_list = time_list[-30:], red_line_list = red_line_list, blue_line_list = blue_line_list, yellow_line_list = yellow_line_list, net_current_list = net_current_list)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
